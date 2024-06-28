324d332f4281070baa7c4c3ee68ca2b33e6d9c4d
sumayukh <sumayukh.sinha@yahoo.com>
2024-06-28 12:48:30 +0530

    Modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: `/math-quiz/`,
+  build: {
+    outDir: "dist",
+    assetsDir: ".",
+  },
 });
